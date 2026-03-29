5e65ea5d5c0a187e94dd9333873ca85e40c03eda
Austin O'Hara <austinaohara@gmail.com>
2026-03-29 14:43:28 -0400

    Hooked fxml to launcher

---
+++ src/main/java/PartPlanApplication.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PartPlanApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main-workspace.fxml"));
+        Scene scene = new Scene(loader.load(), 1000, 700);
+
+        stage.setTitle("PartPlan");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
