d51a0526bae48d202490a16bb237c67dd5758296
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-04-02 19:18:44 -0400

    created Part.java model

---
+++ src/main/java/model/Part.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Part {
+    private int specNumber;
+    private int place;
+    //unknown: specification;
+    private String type;
+    //unknown: bonusTol;
+    /*unknown: measurement;
+        - measurement can be both a float and a string (PASS on spec 6)?
+        - columnControlBar will be controlled by this
+    */
+    private String inspectMethod;
+
+
+}
