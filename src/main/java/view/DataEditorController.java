adf5ec86ece1fa0c5e207620e2a418cb82ad8bee
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-04-02 20:08:50 -0400

    added object vars in DataEditorController.java, some are still unknown

--- src/main/java/view/DataEditorController.java
+++ src/main/java/view/DataEditorController.java
@@ -1,8 +1,50 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import model.InspectionPlan;
 import viewmodel.DataEditorViewModel;
 
 public class DataEditorController {
     private final DataEditorViewModel viewModel = new DataEditorViewModel();
+
+    @FXML
+    private Parent root;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private TableView<InspectionPlan> tableView;
+
+    @FXML
+    private TableColumn<InspectionPlan, Integer> columnSpecNumber;
+
+    @FXML
+    private TableColumn<InspectionPlan, Integer>  columnPlace;
+
+    /*TODO: figure out what variable types to use.
+    *  some of them seem to be images + text. use hbox? or use more columns?*/
+//    @FXML
+//    private TableColumn<InspectionPlan, VALUE>  columnSpecification;
+
+    @FXML
+    private TableColumn<InspectionPlan, String>  columnType;
+
+//    @FXML
+//    private TableColumn<InspectionPlan, VALUE>  columnBonusTol;
+
+//    @FXML
+//    private TableColumn<InspectionPlan, VALUE>  columnMeasurement;
+
+//    // TODO: make control bar a resizable bar of colors
+//    @FXML
+//    private TableColumn<InspectionPlan, VALUE>  columnControlBar;
+
+    @FXML
+    private TableColumn<InspectionPlan, String>  columnInspectMethod;
+
 }
