748ceab44408b5f6a09162fbb34792d13076fd53
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-04-05 03:55:28 -0400

    DataEditorViewModel.java

--- src/main/java/viewmodel/DataEditorViewModel.java
+++ src/main/java/viewmodel/DataEditorViewModel.java
@@ -1,4 +1,25 @@
 package viewmodel;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Part;
+
 public class DataEditorViewModel {
+    private ObservableList<Part> parts = FXCollections.observableArrayList();
+
+    public ObservableList<Part> getParts(){
+        return parts;
+    }
+
+    public void addPart(Part part){
+        parts.add(part);
+    }
+
+    public void removePart(Part part){
+        parts.remove(part);
+    }
+
+
 }
