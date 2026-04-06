cf412ca63e6a000868c8ba3237c6cb36bc7d177f
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-04-05 21:44:19 -0400

    removed constructor parameters from ControlBarCell

--- src/main/java/view/tableCells/ControlBarCell.java
+++ src/main/java/view/tableCells/ControlBarCell.java
@@ -1,14 +1,12 @@
 package view.tableCells;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TableCell;
 import javafx.scene.paint.Color;
 import model.Part;
 
-public class ControlBarCell extends TableCell<Part, Float> {
+public class ControlBarCell extends TableCell<Part, Double> {
 
     private static final double BAR_HEIGHT = 20;
 
@@ -16,13 +14,12 @@ public class ControlBarCell extends TableCell<Part, Float> {
     private static final double YELLOW_RATIO = 0.15;
     private static final double GREEN_RATIO  = 0.40;
 
-    private DoubleProperty tolerance = new SimpleDoubleProperty();
+    private double specDeviation;
+    private double value;
+
     private final Canvas canvas = new Canvas();
 
-    public ControlBarCell(DoubleProperty tolerance) {
-        this.tolerance.bind(tolerance);
-        this.tolerance.addListener((obs, oldT, newT) -> draw());
-
+    public ControlBarCell() {
         tableColumnProperty().addListener((obs, oldCol, newCol) -> {
             if (newCol != null) {
                 newCol.widthProperty().addListener((o, oldW, newW) -> draw());
@@ -64,11 +61,8 @@ public class ControlBarCell extends TableCell<Part, Float> {
         gc.fillRect(redW + yellowW + greenW + yellowW, 0, redW, h);
 
         // --- draw the indicator line ---
-        // value 0.0 = center, -TOLERANCE = far left, +TOLERANCE = far right
-        float value = getItem();
-        double toleranceValue = tolerance.getValue();
-        double clampedValue = Math.max(-toleranceValue, Math.min(toleranceValue, value));
-        double lineX = (w / 2.0) + (clampedValue / toleranceValue) * (w / 2.0);
+        double clampedValue = Math.max(-specDeviation, Math.min(specDeviation, value));
+        double lineX = (w / 2.0) + (clampedValue / specDeviation) * (w / 2.0);
 
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
@@ -76,11 +70,14 @@ public class ControlBarCell extends TableCell<Part, Float> {
     }
 
     @Override
-    protected void updateItem(Float item, boolean empty) {
-        super.updateItem(item, empty);
-        if (empty || item == null) {
+    protected void updateItem(Double value, boolean empty) {
+        super.updateItem(value, empty);
+        if (empty || value == null) {
             setGraphic(null);
         } else {
+            specDeviation = getTableRow().getItem().getSpecNominalSize();
+            this.value = value;
+            System.out.printf("specDev: %f\nvalue: %f", specDeviation, value);
             draw();
             setGraphic(canvas);
         }
