752bd7d0e77ab4aa63ba33053aa42b907e223d99
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-04-25 02:58:58 -0400

    WelcomeController: added method to update UI when signed in or not

--- src/main/java/WelcomeController.java
+++ src/main/java/WelcomeController.java
@@ -3,13 +3,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import repository.AuthRepository;
 import view.AppNavigator;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 public class WelcomeController {
+    public Label welcomeLabel;
+
+    public Button signInButton;
+    public Button signOutButton;
+
+    public Button openPlanEditorButton;
+    public Button openPartEditorButton;
 
     private Stage loginStage;
 
@@ -25,29 +34,33 @@ public class WelcomeController {
         openPlanEditorButton.disableProperty().bind(viewModel.isSignedIn.not());
         openPartEditorButton.disableProperty().bind(viewModel.isSignedIn.not());
 
-        viewModel.isSignedIn.addListener((obs, wasSignedIn, isNowSignedIn) -> {
-            if (isNowSignedIn) {
-                welcomeLabel.setText("Welcome, " + viewModel.getUid());
-            }
-        });
+        signInButton.managedProperty().bind(signInButton.visibleProperty()); // done so that layout isnt changed when invisible
+        signOutButton.managedProperty().bind(signOutButton.visibleProperty());
 
-        if (!viewModel.isSessionValid()){
-            openLoginWindow();
-        }
+        updateSignInUI(viewModel.isSignedIn.get());
+
+        viewModel.isSignedIn.addListener((obs, wasSignedIn, isNowSignedIn) -> {
+            updateSignInUI(isNowSignedIn);
+        });
     }
 
     @FXML
     private void onOpenPlanEditor(ActionEvent event) throws IOException {
-        if (AuthRepository.getToken() == null) {
-            openLoginWindow();
-        } else {
-            AppNavigator.swapRoot((Node) event.getSource(), "/fxml/plan-editor.fxml", "PartPlan - Plan Editor");
-        }
+        AppNavigator.swapRoot((Node) event.getSource(), "/fxml/plan-editor.fxml", "PartPlan - Plan Editor");
     }
 
     @FXML
     private void onOpenPartEditor(ActionEvent event) throws IOException {
         AppNavigator.swapRoot((Node) event.getSource(), "/fxml/inspection-lot-browser.fxml", "PartPlan - Inspection Lots");
+    }
+
+    public void onSignInPressed() throws IOException {
+        openLoginWindow();
+    }
+
+    public void onSignOutPressed() {
+        viewModel.signOut();
+        updateSignInUI(false);
     }
 
     private void openLoginWindow() throws IOException {
@@ -59,9 +72,32 @@ public class WelcomeController {
             loginStage.setAlwaysOnTop(true);
             loginStage.setTitle("Login");
             loginStage.setScene(scene);
+
+            loginStage.setOnCloseRequest(e -> loginStage = null);
+
+            loginStage.setOnHidden(e -> viewModel.refreshSignInStatus());
+
             loginStage.show();
         } else {
             loginStage.toFront();
         }
     }
+
+    private void updateSignInUI(boolean isSignedIn){
+        signInButton.setVisible(!isSignedIn);
+        signOutButton.setVisible(isSignedIn);
+
+        if(isSignedIn){
+            welcomeLabel.setText("Welcome, " + viewModel.getUid());
+        } else {
+            welcomeLabel.setText("Please sign in to continue.");
+        }
+    }
+
+    //test function
+    private void changeExpireTime(){
+        Preferences prefs = Preferences.userRoot().node("partplan/auth");
+        prefs.putLong("tokenExpiry", System.currentTimeMillis() - 1000); // set to 1 second ago
+    }
+
 }
